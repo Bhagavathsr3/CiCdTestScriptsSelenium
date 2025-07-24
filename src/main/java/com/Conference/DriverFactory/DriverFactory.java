20fafa2e46dd836a5ef0f6892143ba2b79749e41
Bhagavathsr3 <bhagavath696@gmail.com>
2025-07-24 12:29:28 +0530

    Chrome options

--- src/main/java/com/Conference/DriverFactory/DriverFactory.java
+++ src/main/java/com/Conference/DriverFactory/DriverFactory.java
@@ -2,6 +2,7 @@ package com.Conference.DriverFactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.Conference.ConfigReader.ConfigReader;
 
@@ -15,7 +16,22 @@ private static WebDriver tldriver;
 		switch (browserValue) {
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
-			 tldriver = new ChromeDriver();
+			 ChromeOptions options = new ChromeOptions();
+
+             // Add headless options only for Linux (like in GitHub Actions)
+             String os = System.getProperty("os.name").toLowerCase();
+             if (os.contains("linux")) {
+                 options.addArguments("--headless=new");
+                 options.addArguments("--no-sandbox");
+                 options.addArguments("--disable-dev-shm-usage");
+                 options.addArguments("--disable-gpu"); // Optional
+                 options.addArguments("--window-size=1920,1080");
+             }
+
+             options.addArguments("--remote-allow-origins=*"); // Optional (for newer Chrome)
+
+             tldriver = new ChromeDriver(options);
+			// tldriver = new ChromeDriver();
 			break;
 
 		default:
